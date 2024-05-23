a8f1bb830d088e421621c4df5d05a3c7084c0fbb
ac57100 <aurelien.cuvelier@gmail.com>
2024-05-23 13:25:25 +0300

    added logs for deployment addresses

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -5,6 +5,7 @@ import {Distributor} from "../src/Distributor.sol";
 import {WarpCore} from "../src/WarpCore.sol";
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 
 import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import {IUniswapV2Factory} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
@@ -23,8 +24,12 @@ contract Deployer is Script {
             IUniFactory(address(5555555555)) //uniswapV2FactoryAddress
         );
 
+        console2.log("WarpCore deployed at: ", address(warpCore));
+
         distributor = new Distributor(
             address(5555555555) //owner
         );
+
+        console2.log("distributor deployed at: ", address(warpCore));
     }
 }
